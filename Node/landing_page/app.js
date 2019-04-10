c2e4bc65db75b381562831faaae41eb0a085ef57
J-Chung-93 <jaegunchung93@gmail.com>
2019-04-10 11:48:55 -0500

    landing page server

--- Node/landing_page/app.js
+++ Node/landing_page/app.js
@@ -9,6 +9,20 @@ var server = http.createServer(function (request, response){
             response.end();
         });
     }
+    else if(request.url === '/ninjas') {
+        fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/dojos/new') {
+        fs.readFile('dojos.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
     else {
         response.writeHead(404);
         response.end('File not found!!!');
