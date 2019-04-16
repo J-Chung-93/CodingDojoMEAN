73d4f51b5a75e740beb6c99e914478be27d1708e
J-Chung-93 <jaegunchung93@gmail.com>
2019-04-16 13:54:01 -0500

    survey form refactored

--- Express/survey_form/server.js
+++ Express/survey_form/server.js
@@ -2,6 +2,10 @@ var express = require("express");
 var app = express();
 var session = require("express-session");
 var bodyParser = require("body-parser");
+const server = app.listen(8000, function() {
+    console.log("listening on port 8000");
+})
+const io = require('socket.io')(server);
 
 app.use(express.static(__dirname + "/static"));
 app.set('views', __dirname + '/views'); 
@@ -13,13 +17,21 @@ app.use(session({
     cookie: {maxAge: 60000}
 }))
 app.use(bodyParser.urlencoded({extended: true}));
+io.on('connection', function(socket){
+    socket.on('form_submit', function(data){
+        var number = Math.floor(Math.random()*1000);
+        var info = JSON.stringify(data);
+        socket.emit("message", {info: `You emitted the following information to the server: ${info}.`});
+        socket.emit("random_number", {random: `Your lucky emitted number by the server is: ${number}.`});
+    })
+})
 
 app.get("/", function(request, response){
     response.render("index");
 })
 app.post("/result", function(request, response){
     console.log("POST DATA \n\n", request.body)
-    response.redirect("/result");
+    response.redirect("/");
 })
 app.get("/result", function(request, response){
     details = {
@@ -29,8 +41,4 @@ app.get("/result", function(request, response){
         comment: request.body.comment
     }
     response.render("results", details);
-})
-
-app.listen(8000, function(){
-    console.log("listening on port 8000");
 })
